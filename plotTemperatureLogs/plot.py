4c3d3fc209c637819e9e963b3cf213c4c239608e
sbond75 <43617712+sbond75@users.noreply.github.com>
2021-10-07 12:03:01 -0500

    Improvements

--- plotTemperatureLogs/plot.py
+++ plotTemperatureLogs/plot.py
@@ -3,10 +3,15 @@ import matplotlib.pyplot as plt
 import clevercsv
 import re
 import os
+from pathlib import Path
 
 # https://stackoverflow.com/questions/3964681/find-all-files-in-a-directory-with-extension-txt-in-python?rq=1
 def get_all_filepaths(root_path, ext):
     for root, dirs, files in os.walk(root_path):
+        files.sort() # Needed for platform-independent order
+        temp = files[2]
+        files[2] = files[1]
+        files[1] = temp
         for filename in files:
             if filename.lower().endswith(ext):
                 yield os.path.join(root, filename)
@@ -26,11 +31,26 @@ for file in get_all_filepaths('./heatTest10-7-2021/', '.csv'):
     # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.assign.html
     df = df.assign(steadyClockTime=lambda x: x.steadyClockTime / 1e9) # Convert to seconds from nanoseconds
     df = df.assign(celcius=lambda x: x.celcius / 1000) # Convert to celcius from thousands
-    
+    df = df[df['steadyClockTime'] <= 500]
+
+    basename = os.path.basename(file)
     subplot = plt.subplot(1, # x dimension
                           1, # y dimension
                           0 + 1)
-    fig = plt.plot(df.loc[:, 'steadyClockTime'], df.loc[:, 'celcius'], label='Heatsinks ' + ' '.join(camel_case_split(os.path.basename(file))).removesuffix(' Time'))
+    #label = 'Heatsinks ' + ' '.join(camel_case_split(basename)).removesuffix(' Time')
+    label = {'timestamps_heatsinksNoFanInYellowPayloadSubscaleHousingAndCloth_unixTime1633576259.csv': 'Heat Sinks',
+             'timestamps_heatsinksWithFanInYellowPayloadSubscaleHousingWithPassiveVenting(SidesOpen)_unixTime1633576936.csv': 'Heat Sinks + Ventilation',
+             'timestamps_heatsinksWithFanInYellowPayloadSubscaleHousingWithBetterCoolingWithFanWorkingInFromSides_unixTime1633579333.csv': 'Heat Sinks + Ventilation + Active Cooling'}[basename]
+    if basename == 'timestamps_heatsinksNoFanInYellowPayloadSubscaleHousingAndCloth_unixTime1633576259.csv':
+        # Modify the df to offset by 15 seconds
+        df = df.assign(steadyClockTime=lambda x: x.steadyClockTime - 15) # Seconds   # https://thispointer.com/pandas-drop-first-n-rows-of-dataframe/#:~:text=To%20delete%20the%20first%20N,default%20values%20i.e.%20(%3A)%20i.e.
+
+        # Remove the first 15 seconds
+        #df = df.iloc[N: , :]
+        df = df[df['steadyClockTime'] >= 15] # https://www.geeksforgeeks.org/drop-rows-from-the-dataframe-based-on-certain-condition-applied-on-a-column/
+    
+    print(label)
+    fig = plt.plot(df.loc[:, 'steadyClockTime'], df.loc[:, 'celcius'], label=label)
     if iIndexNumberY == 0:
         plt.title('Temperatures')
     subplot.set_xlabel('seconds')
@@ -38,4 +58,6 @@ for file in get_all_filepaths('./heatTest10-7-2021/', '.csv'):
     plt.legend()
     #plt.xlim(startX, endX)
 
-plt.show()
+plt.tight_layout()
+#plt.show()
+plt.savefig('out.png', bbox_inches='tight',pad_inches = 0, dpi = 1000)
