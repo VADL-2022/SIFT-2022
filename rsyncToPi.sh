2caf9aca6acb2b41341e160606e14c2ee6506487
sbond75 <43617712+sbond75@users.noreply.github.com>
2021-10-06 21:25:51 -0500

    Trying to fix fscanf

--- rsyncToPi.sh
+++ rsyncToPi.sh
@@ -1,3 +1,3 @@
 actual_path=$(readlink -f "${BASH_SOURCE[0]}")
 script_dir=$(dirname "$actual_path")
-/usr/bin/rsync --backup --archive --verbose --human-readable --progress "$script_dir" pi@raspberrypi.local:/home/pi/VanderbiltRocketTeam/ --exclude='*.o' --exclude='quadcopter' --exclude='example' --exclude='camera' --exclude='.git'
+/usr/bin/rsync --backup --archive --verbose --human-readable --progress "$script_dir"/ pi@raspberrypi.local:/home/pi/VanderbiltRocketTeam/ --exclude='*.o' --exclude='quadcopter' --exclude='example' --exclude='camera' --exclude='.git'
