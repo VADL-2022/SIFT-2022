160fa463f72a0b8237e7abc80299791747446127
sbond75 <43617712+sbond75@users.noreply.github.com>
2021-10-06 21:14:17 -0500

    Trying to fix fscanf

--- rsyncToPi.sh
+++ rsyncToPi.sh
@@ -1,1 +1,1 @@
-/usr/bin/rsync --backup --archive --verbose --human-readable --progress . pi@raspberrypi.local:/home/pi/ --exclude='*.o' --exclude='quadcopter' --exclude='example' --exclude='camera'
+/usr/bin/rsync --backup --archive --verbose --human-readable --progress $(realpath .) pi@raspberrypi.local:/home/pi/ --exclude='*.o' --exclude='quadcopter' --exclude='example' --exclude='camera'
