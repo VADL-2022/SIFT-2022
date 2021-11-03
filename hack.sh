4806858def6b075062768c248c9116a7f1cf03b7
sbond75 <43617712+sbond75@users.noreply.github.com>
2021-11-03 15:17:54 -0500

    wip

--- hack.sh
+++ hack.sh
@@ -1,1 +1,1 @@
-cd sift_anatomy_20141201/ && make clean && make -j8 && cd .. && make clean && make -j8 example && ./example 0
+cd sift_anatomy_20141201/ && make clean && make -j8 && cd .. && make clean && make -j8 sift && ./sift 0
