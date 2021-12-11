2ca1d007e7f62b249b09444b914a28924011cdd8
sbond75 <43617712+sbond75@users.noreply.github.com>
2021-12-11 16:07:43 -0600

    wip integrating IMU code

--- shell.nix
+++ shell.nix
@@ -55,7 +55,7 @@ mkShell {
     
     # VADL2022 "library" #
     (python37m.withPackages (p: with p; [
-      (callPackage ./nix/pyserial.nix {}) #pyserial # https://pyserial.readthedocs.io/en/latest/
+      (callPackage ./pyserial.nix {}) #pyserial # https://pyserial.readthedocs.io/en/latest/
     ]))
     (callPackage ./nix/pigpio.nix {})
     libusb1
