f5667ac98d025f57c492d25cc02d63787ec9e9e2
sbond75 <43617712+sbond75@users.noreply.github.com>
2021-12-12 10:23:12 -0600

    wip integrating driver and etc

--- subscale_imu_and_video_cap.sh
+++ subscale_imu_and_video_cap.sh
@@ -24,7 +24,7 @@ sleep_ () {
 git checkout subscale
 # sha512 Checksum
 sudo -H -u pi /home/pi/.nix-profile/bin/nix-shell --run "make -j4 subscale_exe_release"
-if [ "$dontsleep" == "1" ]; then
+if [ "$dontsleep" != "1" ]; then
     echo "@@@@ Sleeping before takeoff"
     
     # Sleep x seconds:
@@ -34,11 +34,12 @@ echo "@@@@ Starting driver"
 # SIFT start time in milliseconds:
 sudo ./subscale_exe_release --sift-start-time 26000 --imu-record-only --video-capture-too 2>&1 | sudo tee "./dataOutput/$(date +"%Y_%m_%d_%I_%M_%S_%p").video_cap.log.txt" &
 # Stop SIFT after x seconds:
-if [ "$dontsleep" == "1" ]; then
+if [ "$dontsleep" != "1" ]; then
     echo "@@@@ Waiting to stop video cap"
     sleep_ 69 # Ensure you don't subtract the above times, since we run the above sleep in the background.
 fi
 echo "@@@@ Stopping video cap"
+set +e
 pkill -SIGINT subscale
 pkill -SIGINT videoCapture # Kill python video capture
 # sha512 Checksum
