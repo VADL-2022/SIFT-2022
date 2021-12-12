c88a1fa1c2e7a4a2a2d370f1d2c1b594f6d6a3c4
sbond75 <43617712+sbond75@users.noreply.github.com>
2021-12-11 23:30:56 -0600

    wip integrating IMU code

--- subscale.sh
+++ subscale.sh
@@ -2,6 +2,11 @@
 set -eE  # same as: `set -o errexit -o errtrace`
 trap 'echo "@@@@@@@@@@@@@@@@@@@@@@@@@@ Failed to prepare subscale @@@@@@@@@@@@@@@@@@@@@@@@@@"' ERR 
 
+sleep_ () {
+    echo "Sleeping for $@"
+    sleep "$@"
+}
+
 git checkout subscale
 # TODO: checkout submodules
 make -j4 sift_exe_release_commandLine
@@ -9,13 +14,13 @@ make -j4 sift_exe_release_commandLine
 make -j4 subscale_exe_release
 echo "@@@@ Sleeping before takeoff"
 # Sleep x seconds:
-sleep 30
+sleep_ 30
 echo "@@@@ Starting driver"
 # SIFT start time in milliseconds:
-sudo ./subscale_exe_release --sift-start-time 26000 --sift-only | sudo tee "./dataOutput/$(date +"%Y_%m_%d_%I_%M_%p").log.txt" &
+sudo ./subscale_exe_release --sift-start-time 26000 --sift-only 2>&1 | sudo tee "./dataOutput/$(date +"%Y_%m_%d_%I_%M_%p").log.txt" &
 # Stop SIFT after x seconds:
 echo "@@@@ Waiting to stop SIFT"
-sleep $((69-26)) # Ensure subtract times, except first sleep, from the above in seconds
+sleep_ $((69-26)) # Ensure to subtract times, except first sleep, from the above in seconds
 echo "@@@@ Stopping SIFT"
 pkill -SIGINT sift
 # sha512 Checksum
