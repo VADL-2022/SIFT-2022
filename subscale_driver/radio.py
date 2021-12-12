fe718d4991265e9a34f447fff4671e88d42edc2d
sbond75 <43617712+sbond75@users.noreply.github.com>
2021-12-11 22:27:27 -0600

    wip integrating IMU code

--- subscale_driver/radio.py
+++ subscale_driver/radio.py
@@ -1,9 +1,13 @@
 import serial
 import random
+d = [x for x in range(300)] # range() excludes value given
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1) # gpio14
     ser.reset_input_buffer()
     
-    while True:
-        Data = random.randint(1,4)
+    while i < len(d):
+        #Data = random.randint(1,4)
+        data = d[i]
         ser.write(str(data).encode('utf-8'))
+        
+        i+=1
