3f35f106a8024243ba82d3c2766226de7af41e79
sbond75 <43617712+sbond75@users.noreply.github.com>
2021-12-11 21:12:32 -0600

    wip integrating IMU code

--- subscale_driver/VADL2022.cpp
+++ subscale_driver/VADL2022.cpp
@@ -53,7 +53,7 @@ State g_state = State_WaitingForTakeoff;
 //const char *sift_args[] = { "./sift_exe_release_commandLine","--main-mission", "--sift-params","-C_edge","2", "--sleep-before-running",(timeFromTakeoffToMainDeploymentAndStabilization), (const char *)0 };
 bool startDelayedSIFT() {
     std::string s = R"(import subprocess
-p = subprocess.Popen(["./sift_exe_release_commandLine","--main-mission", "--sift-params","-C_edge","2", "--sleep-before-running",)" + std::string(timeFromTakeoffToMainDeploymentAndStabilization) + R"("])
+p = subprocess.Popen(["echo","aaaaaaaaaaaaaA"]) #["./sift_exe_release_commandLine","--main-mission", "--sift-params","-C_edge","2", "--sleep-before-running",)" + std::string(timeFromTakeoffToMainDeploymentAndStabilization) + R"("])
 # Above process runs asynchronously unless you wait:
 p.wait()
 )";
