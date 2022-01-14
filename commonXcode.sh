5eb1ff5759b58e80cf3bcd55f7426b885aed3f9e
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-01-13 20:21:08 -0600

    Cleanup after merge

--- commonXcode.sh
+++ commonXcode.sh
@@ -16,7 +16,7 @@ XCCONFIG="Config.xcconfig"
 #truncate –s 0 "$XCCONFIG"
 echo `which pkg-config`
 echo "$OPENCV_CFLAGS"
-IFLAGS="$(pkg-config --cflags-only-I opencv4 libpng) -I"'$(SRCROOT)/../'"$SIFT_SRC -I/nix/store/zflx47lr00hipvkl5nncd2rnpzssnni6-backward-1.6/include"
+IFLAGS="$(pkg-config --cflags-only-I opencv4 libpng jemalloc gl glew IL) -I"'$(SRCROOT)/../'"$SIFT_SRC -I/nix/store/zflx47lr00hipvkl5nncd2rnpzssnni6-backward-1.6/include -I/nix/store/i0rmp3app7yqd37ihgxlx9c3lwsj16kq-opencl-headers-2020.06.16/include"
 echo "$IFLAGS"
 
 # Use a heredoc:
@@ -25,11 +25,14 @@ CXXFLAGS = -std=c++17 \$(CXXFLAGS)
 OPENCV_CFLAGS=`pkg-config --cflags opencv4 libpng`
 HEADER_SEARCH_PATHS=$(echo -n "$IFLAGS" | awk 'BEGIN { ORS=" "; RS = " " } ; { sub(/^-I/, ""); print $0 }')
 
-OTHER_LDFLAGS = -lm -lpthread `pkg-config --libs opencv4 libpng` -L/nix/store/g5w2hcjkax12ixx8wq1g6r6nqri15ggx-libunwind-7.1.0/lib // Works but segfaults: -Wl,-alias,_malloc,vadlTemp -Wl,-alias,___wrap_malloc,_malloc -Wl,-alias,vadlTemp,___real_malloc -Wl,-alias,_free,vadlTemp2 -Wl,-alias,___wrap_free,_free -Wl,-alias,vadlTemp2,___real_free //-Wl,-alias,_malloc,___wrap_malloc -Wl,-alias,_free,___wrap_free -Wl,-alias,___real_malloc,_malloc -Wl,-alias,___real_free,_free //-Wl,--wrap,malloc -Wl,--wrap,free //-ljpeg -lrt -lm
+OTHER_LDFLAGS = -lm -lpthread `pkg-config --libs opencv4 libpng jemalloc gl glew glut IL` -framework OpenCL -L/nix/store/g5w2hcjkax12ixx8wq1g6r6nqri15ggx-libunwind-7.1.0/lib // Works but segfaults: -Wl,-alias,_malloc,vadlTemp -Wl,-alias,___wrap_malloc,_malloc -Wl,-alias,vadlTemp,___real_malloc -Wl,-alias,_free,vadlTemp2 -Wl,-alias,___wrap_free,_free -Wl,-alias,vadlTemp2,___real_free //-Wl,-alias,_malloc,___wrap_malloc -Wl,-alias,_free,___wrap_free -Wl,-alias,___real_malloc,_malloc -Wl,-alias,___real_free,_free //-Wl,--wrap,malloc -Wl,--wrap,free //-ljpeg -lrt -lm
 
 CLANG_CXX_LIBRARY=libc++
 CLANG_CXX_LANGUAGE_STANDARD=c++17
 
-GCC_PREPROCESSOR_DEFINITIONS = \$(inherited) BACKWARD_HAS_UNWIND=1 USE_COMMAND_LINE_ARGS
-//USE_PTR_INC_MALLOC
+//impl = SIFT_IMPL=SIFTGPU SIFTGPU_ // <-- Be sure to change both of these defines for an impl change
+impl = SIFT_IMPL=SIFTAnatomy SIFTAnatomy_
+//tests = SIFTGPU_TEST
+//etcConfig = USE_PTR_INC_MALLOC=1
+GCC_PREPROCESSOR_DEFINITIONS = \$(inherited) BACKWARD_HAS_UNWIND=1 USE_COMMAND_LINE_ARGS WINDOW_PREFER_GLUT CL_SIFTGPU_ENABLED \$(impl) \$(tests) \$(etcConfig)
 InputComesFromHERE
