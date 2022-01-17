e9ddb40a4d8f1294c05054328bfe0825629e2c36
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-01-17 15:56:42 -0600

    compareNatSort command-line tool

--- mergeVideosInDataOutput.sh
+++ mergeVideosInDataOutput.sh
@@ -5,7 +5,7 @@ DIR=$(dirname "${BASH_SOURCE[0]}")   # Get the directory name
 DIR=$(realpath "${DIR}")    # Resolve its full path if need be
 
 # https://stackoverflow.com/questions/28922352/how-can-i-merge-all-the-videos-in-a-folder-to-make-a-single-video-file-using-ffm/37756628
-output="$(basename "$1")/output.mp4"
+output="$(dirname "$1")/output.mp4"
 #ffmpeg -f concat -i <("$DIR/compareNatSort/compareNatSort" "$1" | sed 's:\ :\\\ :g'| sed 's/^/file /') -c copy "$output"
 
 "$DIR/compareNatSort/compareNatSort" "$1" | sed 's:\ :\\\ :g'| sed 's/^/file /' > "$DIR/fl.txt"
