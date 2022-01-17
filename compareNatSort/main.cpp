e09ae20f9d77b577a3dbc23da0f1e61b2b20334b
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-01-17 15:40:29 -0600

    compareNatSort command-line tool

--- compareNatSort/main.cpp
+++ compareNatSort/main.cpp
@@ -9,31 +9,35 @@ namespace fs = std::filesystem;
 
 #include "common.hpp"
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   // std::cout << argc << std::endl;
   // std::cout << argv << std::endl;
-  //FolderDataSource f(argc, argv, 0);
-  //std::cout << f.files.size() << std::endl;
+  // FolderDataSource f(argc, argv, 0);
+  // std::cout << f.files.size() << std::endl;
   // for (auto& file : f.files) {
   //   std::cout << "1" << std::endl;
   // }
 
-    std::vector<std::string> files;
+  std::vector<std::string> files;
   std::string folderPath = argv[1];
-  for (const auto & entry : fs::directory_iterator(folderPath)) {
-        // Ignore files we treat specially:
-        if (entry.is_directory() || endsWith(entry.path().string(), ".keypoints.txt") || endsWith(entry.path().string(), ".matches.txt") || entry.path().filename().string() == ".DS_Store") {
-            continue;
-        }
-        files.push_back(entry.path().string());
+  for (const auto &entry : fs::directory_iterator(folderPath)) {
+    // Ignore files we treat specially:
+    // if (entry.is_directory() || endsWith(entry.path().string(),
+    // ".keypoints.txt") || endsWith(entry.path().string(), ".matches.txt") ||
+    // entry.path().filename().string() == ".DS_Store") {
+    //     continue;
+    // }
+    if (!endsWith(entry.path().string(), ".keypoints.txt")) {
+      continue;
     }
-
-    // https://stackoverflow.com/questions/9743485/natural-sort-of-directory-filenames-in-c
-    std::sort(files.begin(),files.end(),compareNat);
-
-  for (auto& file : files) {
-    //std::cout << "1" << std::endl;
-     std::cout << file << std::endl;
+    files.push_back(entry.path().string());
   }
-    
+
+  // https://stackoverflow.com/questions/9743485/natural-sort-of-directory-filenames-in-c
+  std::sort(files.begin(), files.end(), compareNat);
+
+  for (auto &file : files) {
+    // std::cout << "1" << std::endl;
+    std::cout << file << std::endl;
+  }
 }
