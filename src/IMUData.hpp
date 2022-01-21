a5f3783c7a1f8f41f3ee0ae807c1f97b0b335a82
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-01-21 16:52:34 -0600

    LD_LIBRARY_PATH workaround attempt

---
+++ src/IMUData.hpp
@@ -0,0 +1,13 @@
+//
+//  IMUData.hpp
+//  SIFT
+//
+//  Created by VADL on 1/20/22.
+//  Copyright © 2022 VADL. All rights reserved.
+//
+
+#ifndef IMUData_hpp
+#define IMUData_hpp
+
+
+#endif /* IMUData_hpp */
