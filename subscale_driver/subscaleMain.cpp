47762e557fc68b6bd791128bc0bd6e5fd5d6fd75
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-01-22 22:57:37 -0600

    Python signal handlers wip

--- subscale_driver/subscaleMain.cpp
+++ subscale_driver/subscaleMain.cpp
@@ -4,15 +4,23 @@
 #include "VADL2022.hpp"
 
 Queue<QueuedFunction, 8> mainDispatchQueue;
+std::atomic<bool> isRunningPython;
 
 int main(int argc, char **argv) {
   VADL2022 vadl(argc, argv);
 
   QueuedFunction f;
   while (true) {
+    std::cout << "Main dispatch queue waiting for next function..." << std::endl;
     mainDispatchQueue.dequeue(&f);
     std::cout << "Main dispatch queue executing function with description: " << f.second << std::endl;
-    f.first();
+    if (f.type == QueuedFunctionType::Python) {
+      isRunningPython = true;
+    }
+    f.f();
+    if (f.type == QueuedFunctionType::Python) {
+      isRunningPython = false;
+    }
   }
 
   PyRun_SimpleString("input('Done')");
--- subscale_driver/subscaleMain.hpp
+++ subscale_driver/subscaleMain.hpp
@@ -1,5 +1,16 @@
 #include "../src/Queue.hpp"
 #include <functional>
 
-using QueuedFunction = std::pair<std::function<void(void)>, const char*>;
+enum QueuedFunctionType {
+  Misc,
+  Python
+};
+
+struct QueuedFunction {
+  std::function<void(void)> f;
+  const char *description;
+  QueuedFunctionType type;
+};
+
 extern Queue<QueuedFunction, 8> mainDispatchQueue;
+extern std::atomic<bool> isRunningPython;
