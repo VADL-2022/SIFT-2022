6931c486a5642e67a042a67fffe50f578dbf4a47
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-01-29 12:04:59 -0600

    driver script

--- subscale.sh
+++ subscale.sh
@@ -110,9 +110,9 @@ if [ "$mode" == "sift" ]; then
     realFlight="--sift-start-time 20000 --takeoff-g-force 5 --main-deployment-g-force 1 --backup-sift-stop-time $(($mainDeploymentToTouchDown-10000))"
     testing="--sift-start-time 0 --backup-sift-stop-time 20000"
     extraArgs="$testing"
-    ./subscale_exe_release --sift-params '-C_edge 2 -delta_min 0.6' --backup-takeoff-time 0 --backup-sift-start-time 10000 $extraArgs 2>&1 | sudo tee "./dataOutput/$(date +"%Y_%m_%d_%I_%M_%S_%p").$mode""log.txt"
+    ./subscale_exe_release --sift-params '-C_edge 2 -delta_min 0.6' --backup-takeoff-time 0 --backup-sift-start-time 10000 $extraArgs 2>&1 | tee "./dataOutput/$(date +"%Y_%m_%d_%I_%M_%S_%p").$mode""log.txt" | tee <(python3 "subscale_driver/radio.py" 1)
 else
-    ./subscale_exe_release --video-capture --sift-start-time "$siftStart" 2>&1 | sudo tee "./dataOutput/$(date +"%Y_%m_%d_%I_%M_%S_%p").$mode""log.txt"
+    ./subscale_exe_release --video-capture --sift-start-time "$siftStart" 2>&1 | tee "./dataOutput/$(date +"%Y_%m_%d_%I_%M_%S_%p").$mode""log.txt"
 fi
 set +e
 cleanup
--- subscale_driver/radio.py
+++ subscale_driver/radio.py
@@ -1,11 +1,13 @@
 import serial
 import random
 import time
-d = [x for x in range(300)] # range() excludes value given
-if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1) # gpio14
-    ser.reset_input_buffer()
 
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1) # gpio14
+ser.reset_input_buffer()
+
+def test():
+    d = [x for x in range(300)] # range() excludes value given
+    
     i=0
     while i < len(d):
         #Data = random.randint(1,4)
@@ -13,6 +15,20 @@ if __name__ == '__main__':
         res = str(data).encode('utf-8')
         print(res)
         ser.write(res)
-        time.sleep(0.5)
-        
+        time.sleep(0.01)
+
         i+=1
+
+
+useStdin = (sys.argv[1] == '1') if len(sys.argv) > 1 else False
+
+if __name__ == '__main__':
+    if useStdin:
+        line = input()
+        while line:
+            res = str(line).encode('utf-8')
+            ser.write(res)
+            
+            line = input()
+    else:
+        test()
