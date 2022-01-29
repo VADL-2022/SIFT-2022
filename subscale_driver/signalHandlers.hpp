6da4062f9b0bc735a5d0f2ad2b08824a5d32ef5c
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-01-28 23:37:42 -0600

    Stopping SIFT on IMU stuff

--- subscale_driver/signalHandlers.hpp
+++ subscale_driver/signalHandlers.hpp
@@ -6,7 +6,5 @@ extern void ctrlC(int s, siginfo_t *si, void *arg);
 
 extern void terminate_handler(bool backtrace);
 
-extern void segfault_sigaction(int signal, siginfo_t *si, void *arg);
-
 // Installs signal handlers for the current thread.
 extern void installSignalHandlers();
