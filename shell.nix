62c7fd9799ae02ddcf478f42f9ba60de7ed7c21b
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-01-29 20:33:07 -0600

    LIS331HH IMU stuff

--- shell.nix
+++ shell.nix
@@ -70,8 +70,8 @@ mkShell {
       #matplotlib
 
       # For LIS331HH IMU
-      (callPackage ./nix/smbus2.nix {})
-      (callPackage ./nix/rpi_gpio.nix {})
+      (callPackage ./nix/smbus2.nix {python37m.buildPythonPackage})
+      (callPackage ./nix/rpi_gpio.nix {python37m.buildPythonPackage})
       (lib.optional (stdenv.hostPlatform.isLinux) (callPackage ./nix/smbus2.nix {}))
       (lib.optional (stdenv.hostPlatform.isLinux) (callPackage ./nix/rpi_gpio.nix {}))
     ]))
