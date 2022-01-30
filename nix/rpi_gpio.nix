7a8dcde5ef43c873a6caafbe3d020f6bbc7c6656
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-01-29 20:30:02 -0600

    LIS331HH IMU stuff

--- nix/rpi_gpio.nix
+++ nix/rpi_gpio.nix
@@ -1,8 +1,8 @@
 # Based on https://gist.github.com/taktoa/51af642c37292e6b15d3bd30c4f9c6ff
 
-{ stdenv, fetchFromGitHub, buildPythonPackage }:
+{ stdenv, fetchFromGitHub }:
 
-buildPythonPackage {
+stdenv.buildPythonPackage {
   pname = "rpi_gpio";
   version = "0.7.0";
   src = ./RPi.GPIO-0.7.0;
