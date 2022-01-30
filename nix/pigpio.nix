92d047f8e687e5dfa061e6808d6d7708a0a38970
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-01-29 20:36:36 -0600

    LIS331HH IMU stuff

--- nix/pigpio.nix
+++ nix/pigpio.nix
@@ -1,8 +1,8 @@
 # Based on https://gist.github.com/taktoa/51af642c37292e6b15d3bd30c4f9c6ff
 
-{ lib, buildPythonPackage, cmake, fetchFromGitHub }:
+{ stdenv, fetchFromGitHub, cmake }:
 
-buildPythonPackage rec {
+stdenv.mkDerivation {
   pname = "pigpio";
   version = "79";
   src = fetchFromGitHub {
@@ -14,3 +14,21 @@ buildPythonPackage rec {
   buildInputs = [cmake];
   patches = [ ./pigpio.patch2.patch ];
 }
+
+# # Based on https://gist.github.com/taktoa/51af642c37292e6b15d3bd30c4f9c6ff
+# 
+# { lib, buildPythonPackage, cmake, fetchFromGitHub }:
+# 
+# buildPythonPackage rec {
+#   pname = "pigpio";
+#   version = "79";
+#   src = fetchFromGitHub {
+#     owner  = "joan2937";
+#     repo   = "pigpio";
+#     rev    = "c33738a320a3e28824af7807edafda440952c05d";
+#     sha256 = "0wgcy9jvd659s66khrrp5qlhhy27464d1pildrknpdava19b1r37";
+#   };
+#   buildInputs = [cmake];
+#   patches = [ ./pigpio.patch2.patch ];
+# }
+# 
