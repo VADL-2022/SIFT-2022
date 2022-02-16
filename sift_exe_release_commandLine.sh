8a75be0bca1c85ffd850e3c997e8381ad3e3334e
Kai Malcolm <65028609+kdmalc@users.noreply.github.com>
2022-02-16 12:57:03 -0600

    Switching flags: main mission->imu record only

--- sift_exe_release_commandLine.sh
+++ sift_exe_release_commandLine.sh
@@ -1,2 +1,3 @@
 export DISPLAY=:0.0
-./sift_exe_release_commandLine --main-mission --video-file-data-source
+// ./sift_exe_release_commandLine --main-mission --video-file-data-source
+./sift_exe_release_commandLine --imu-record-only --video-file-data-source
