3d868bb0645e6001a74fe4aafa046f9e2a53bf9b
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-02-18 16:51:40 -0600

    Verbose settings

--- subscale_driver/VADL2022.cpp
+++ subscale_driver/VADL2022.cpp
@@ -186,7 +186,7 @@ bool startDelayedSIFT_fork(const char *sift_args[], size_t sift_args_size, bool 
     std::string(timeAfterMainDeployment) //+
     //std::string(" --no-preview-window") // --video-file-data-source
     + (useIMU ? (std::string(" --subscale-driver-fd ") + std::to_string(fd[0])) : "")
-    + (verboseSIFTFD ? (std::string(" --verbose")))
+    + (verboseSIFTFD ? (std::string(" --verbose")) : "")
   ;
   
   printf("Forking with bash command: %s\n", siftCommandLine.c_str());
