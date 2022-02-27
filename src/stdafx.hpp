e1d811a64eae92a8b3a3cb0ab016c1a5e6b0904e
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-02-26 22:29:31 -0600

    pch wip

---
+++ src/stdafx.hpp
@@ -0,0 +1,22 @@
+//
+//  stdafx.hpp.pch
+//  SIFT
+//
+//  Created by VADL on 2/26/22.
+//  Copyright © 2022 VADL. All rights reserved.
+//
+
+#ifndef stdafx_hpp_h
+#define stdafx_hpp_h
+
+// Precompiled header for compilation speed boost.
+// For C++ (or C++-compatible C header) includes only.
+
+#include "opencv_common.hpp"
+#include "opencv_highgui_common.hpp"
+#include "opencv_xfeatures2d_common.hpp"
+#include "common.hpp"
+#include "../vn_sensors_common.hpp"
+#include "Queue.hpp"
+
+#endif /* stdafx_hpp_h */
