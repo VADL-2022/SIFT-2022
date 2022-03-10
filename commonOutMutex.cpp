784008f3b5c5fe2cf5e7ae064d1f26c9186e1417
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-03-10 17:55:25 -0600

    compilation fixing

---
+++ commonOutMutex.cpp
@@ -0,0 +1,13 @@
+//
+//  commonOutMutex.cpp
+//  SIFT
+//
+//  Created by VADL on 3/10/22.
+//  Copyright © 2022 VADL. All rights reserved.
+//
+
+#include "commonOutMutex.hpp"
+
+namespace common {
+    std::mutex outMutex;
+}
