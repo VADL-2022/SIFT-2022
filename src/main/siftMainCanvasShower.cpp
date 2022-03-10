f9dd8cbd1a946f91e9ac30d3dd9ca44c0b3c6444
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-03-10 17:38:55 -0600

    python compile fix

---
+++ src/main/siftMainCanvasShower.cpp
@@ -0,0 +1,9 @@
+//
+//  siftMainCanvasShower.cpp
+//  SIFT
+//
+//  Created by VADL on 3/10/22.
+//  Copyright © 2022 VADL. All rights reserved.
+//
+
+#include "siftMainCanvasShower.hpp"
---
+++ src/main/siftMainCanvasShower.hpp
@@ -0,0 +1,14 @@
+//
+//  siftMainCanvasShower.hpp
+//  SIFT
+//
+//  Created by VADL on 3/10/22.
+//  Copyright © 2022 VADL. All rights reserved.
+//
+
+#ifndef siftMainCanvasShower_hpp
+#define siftMainCanvasShower_hpp
+
+#include <stdio.h>
+
+#endif /* siftMainCanvasShower_hpp */
