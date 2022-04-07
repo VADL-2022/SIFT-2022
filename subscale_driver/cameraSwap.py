cbd89d5322451a612eac4f0197b348071aa9b952
EthanMayer <ethan.h.mayer@vanderbilt.edu>
2022-04-06 21:57:29 -0500

    camera swap fix

--- subscale_driver/cameraSwap.py
+++ subscale_driver/cameraSwap.py
@@ -4,7 +4,7 @@ import sys
 
 pi = pigpio.pi()
 
-cameraDown = (sys.argv[1] == '1') if len(sys.argv) > 1 else None
+cameraDown = int(sys.argv[1]) if len(sys.argv) > 1 else None
 
 if cameraDown is 0:
     # First camera
