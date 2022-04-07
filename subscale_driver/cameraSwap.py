b54aed020714fd500f5d290df129b3a8a33e2aee
EthanMayer <ethan.h.mayer@vanderbilt.edu>
2022-04-06 21:51:34 -0500

    camera swap fix

--- subscale_driver/cameraSwap.py
+++ subscale_driver/cameraSwap.py
@@ -1,15 +1,19 @@
 import pigpio
 import time
+import sys
 
-# Swap to second camera
 pi = pigpio.pi()
-pi.set_mode(26, pigpio.INPUT) # Set pin 26 to input
-pi.set_pull_up_down(26, pigpio.PUD_DOWN) # Set pin 26 to pull down resistor
 
-time.sleep(6)
+cameraDown = (sys.argv[1] == '1') if len(sys.argv) > 1 else None
 
-pi.set_pull_up_down(26, pigpio.PUD_OFF) # Clear pull down resistor on pin 26
-pi.set_mode(26, pigpio.OUTPUT) # Set pin 26 to output
-pi.write(26, 1) # Set pin 26 to high
+if cameraDown is 0:
+    # First camera
+    pi.set_pull_up_down(26, pigpio.PUD_OFF) # Clear pull down resistor on pin 26
+    pi.set_mode(26, pigpio.OUTPUT) # Set pin 26 to output
+    pi.write(26, 1) # Set pin 26 to high
+else:
+    # Swap to second camera
+    pi.set_mode(26, pigpio.INPUT) # Set pin 26 to input
+    pi.set_pull_up_down(26, pigpio.PUD_DOWN) # Set pin 26 to pull down resistor
 
 exit()
