227b523e3ca0bc33d27550149e9eb607b45224ed
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-04-16 09:20:40 -0500

    altitude for high bad values

--- driver/VADL2022.cpp
+++ driver/VADL2022.cpp
@@ -495,8 +495,10 @@ double updateRelativeAltitude(LOG_T log, bool showAltitudeOnce, bool actuallyUpd
   double kilopascals = log->mImu->pres;
   double altitudeFeet = computeAltitude(kilopascals);
   if (actuallyUpdateAltitude) {
-    if (onGroundAltitude == DBL_MIN) {
+    if (onGroundAltitude == DBL_MIN || onGroundAltitude > 50000/*145366 was reported once, so this catches that and filters it out so that the relative altitude grabbing can re-latch onto a different value*/) {
       onGroundAltitude = altitudeFeet;
+      { out_guard();
+        std::cout << "Latching onto relativeAltitude: " << onGroundAltitude << " ft" << std::endl; }
       numAltitudes = 1;
     }
     else {
