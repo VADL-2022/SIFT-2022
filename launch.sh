f2b06c846582f4cca45dc49cf9ff4e16a9a79b15
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-04-16 09:06:37 -0500

    sift args fix

--- launch.sh
+++ launch.sh
@@ -170,11 +170,11 @@ launchAngle=$2
 siftAllowanceForStopping=0 # Time to take away from mainDescentTime for stopping SIFT in backupSIFTStopTime. Larger values stop SIFT sooner.
 backupSIFTStopTime="$(($mainDescentTime-$siftAllowanceForStopping))" # Originally we were going to stop SIFT on altitude data. But we're unsure about altitude data being reliable, so we don't use it to stop SIFT, and there's a fallback via backupSIFTStopTime.
 
-forePayloadArgs="--video-capture --LIS331HH-imu-calibration-file "driver/LIS331HH_calibration/LOG_20220129-183224.csv" --time-to-apogee $timeToApogee"
+forePayloadArgs="--video-capture --LIS331HH-imu-calibration-file "driver/LIS331HH_calibration/LOG_20220129-183224.csv""
 
 siftPayloadArgs="$siftPayloadArgs --time-to-main-deployment $timeToMainDeployment --time-for-main-stabilization $mainStabilizationTime --main-descent-time $backupSIFTStopTime --emergency-main-deployment-g-force 20 --main-deployment-altitude $mainDeploymentAltitude --launch-box $launchBox --launch-angle $launchAngle"
 
-realFlight="--takeoff-g-force $takeoffGForce --landing-g-force $landingGForce --backup-takeoff-time $backupTakeoffTime --time-to-meco $timeToMeco"
+realFlight="--takeoff-g-force $takeoffGForce --landing-g-force $landingGForce --backup-takeoff-time $backupTakeoffTime --time-to-meco $timeToMeco --time-to-apogee $timeToApogee"
 
 # Add overrides to realFlight for testing:
 testing="$realFlight --takeoff-g-force 1 --landing-g-force 1"
@@ -187,7 +187,7 @@ extraArgs="$realFlight"
 
 
 if [ "$mode" == "sift" ]; then
-    ./$exe $extraArgs $siftPayloadArgs --extra-sift-exe-args "--no-sky-detection --fps $fps --no-preview-window --finish-rest-always --save-first-image" --sift-params '-C_edge 2 -delta_min 0.6' 2>&1 | tee "./dataOutput/$(date +"%Y_%m_%d_%I_%M_%S_%p").$mode""log.txt" #| tee <(python3 "driver/radio.py" 1)
+    ./$exe $siftPayloadArgs --extra-sift-exe-args "--no-sky-detection --fps $fps --no-preview-window --finish-rest-always --save-first-image" --sift-params '-C_edge 2 -delta_min 0.6' $extraArgs 2>&1 | tee "./dataOutput/$(date +"%Y_%m_%d_%I_%M_%S_%p").$mode""log.txt" #| tee <(python3 "driver/radio.py" 1)
 else
     #./subscale_exe_release --video-capture --time-for-main-stabilization "$siftStart" 2>&1 | tee "./dataOutput/$(date +"%Y_%m_%d_%I_%M_%S_%p").$mode""log.txt"
     ./$exe $forePayloadArgs $extraArgs 2>&1 | tee "./dataOutput/$(date +"%Y_%m_%d_%I_%M_%S_%p").$mode""log.txt"
