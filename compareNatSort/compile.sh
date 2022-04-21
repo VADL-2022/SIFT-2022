a16c8db2cb8cf737b464a64ca53fc0f474d7f503
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-04-21 14:00:16 -0500

    sift.py integrated into driver using --python-sift

--- compareNatSort/compile.sh
+++ compareNatSort/compile.sh
@@ -1,4 +1,4 @@
 if [[ main.cpp -nt compareNatSort ]]; then
     ../tools/compileSubproject.sh main.cpp -o compareNatSort
 fi
-./compareNatSort . ".cpp" | grep asdasd # tests if it works based on exit code
+./compareNatSort . ".cpp" | grep main # tests if it works based on exit code
