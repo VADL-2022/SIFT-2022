e99df9ba9cd220455da02009cf8189bd986dfbdf
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-04-21 14:01:34 -0500

    sift.py integrated into driver using --python-sift

--- compareNatSort/compile.sh
+++ compareNatSort/compile.sh
@@ -1,4 +1,8 @@
 if [[ main.cpp -nt compareNatSort ]]; then
+    echo "Recompiling compareNatSort"
     ../tools/compileSubproject.sh main.cpp -o compareNatSort
+else
+    echo "compareNatSort is up-to-date"
 fi
 ./compareNatSort . ".cpp" | grep main # tests if it works based on exit code
+echo "compareNatSort and grep exit code: $?"
